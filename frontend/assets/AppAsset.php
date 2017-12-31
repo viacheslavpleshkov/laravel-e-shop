d5614d6f2f37b407ed9b2ed2aed1f508404d5d97
slava-pleshkov <slavapleshkovcom@gmail.com>
2017-12-31 14:07:42 +0200

    v-0-1-1

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,12 +12,23 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700',
+        'css/style.css',
+        'css/owl.carousel.css',
+        'css/owl.carousel.css',
     ];
     public $js = [
+        'js/jquery.min.js',
+        'js/modernizr.custom.28468.js',
+        'js/jquery.cslider.js',
+        'js/owl.carousel.js',
+        'js/move-top.js',
+        'js/easing.js',
+        'js/responsive.menu.js',
+
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+//        'yii\web\YiiAsset',
+//        'yii\bootstrap\BootstrapAsset',
     ];
 }
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,52 +2,191 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Yii2-Aditii';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+<!-- start slider -->
+<div id="da-slider" class="da-slider">
+    <div class="da-slide">
+        <h2>welcome to aditii</h2>
+        <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
+        <a href="details.html" class="da-link">shop now</a>
+        <div class="da-img"><img src="images/slider1.png" alt="image01" /></div>
     </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+    <div class="da-slide">
+        <h2>Easy management</h2>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+        <a href="details.html" class="da-link">shop now</a>
+        <div class="da-img"><img src="images/slider2.png" alt="image01" /></div>
+    </div>
+    <div class="da-slide">
+        <h2>Revolution</h2>
+        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+        <a href="details.html" class="da-link">shop now</a>
+        <div class="da-img"><img src="images/slider3.png" alt="image01" /></div>
+    </div>
+    <div class="da-slide">
+        <h2>Quality Control</h2>
+        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+        <a href="details.html" class="da-link">shop now</a>
+        <div class="da-img"><img src="images/slider4.png" alt="image01" /></div>
+    </div>
+    <nav class="da-arrows">
+        <span class="da-arrows-prev"></span>
+        <span class="da-arrows-next"></span>
+    </nav>
+</div>
+</div>
+<!----start-cursual---->
+<div class="wrap">
+    <!----start-img-cursual---->
+    <div id="owl-demo" class="owl-carousel">
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c1.jpg" alt="Lazy Owl Image">
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="cau_left">
+                <h4><a href="details.html">branded shoes</a></h4>
+                <a href="details.html" class="btn">shop</a>
             </div>
         </div>
-
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+            </div>
+            <div class="cau_left">
+                <h4><a href="details.html">branded tees</a></h4>
+                <a href="details.html" class="btn">shop</a>
+            </div>
+        </div>
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c3.jpg" alt="Lazy Owl Image">
+            </div>
+            <div class="cau_left">
+                <h4><a href="details.html">branded jeens</a></h4>
+                <a href="details.html" class="btn">shop</a>
+            </div>
+        </div>
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+            </div>
+            <div class="cau_left">
+                <h4><a href="details.html">branded tees</a></h4>
+                <a href="details.html" class="btn">shop</a>
+            </div>
+        </div>
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c1.jpg" alt="Lazy Owl Image">
+            </div>
+            <div class="cau_left">
+                <h4><a href="details.html">branded shoes</a></h4>
+                <a href="details.html" class="btn">shop</a>
+            </div>
+        </div>
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c2.jpg" alt="Lazy Owl Image">
+            </div>
+            <div class="cau_left">
+                <h4><a href="details.html">branded tees</a></h4>
+                <a href="details.html" class="btn">shop</a>
+            </div>
+        </div>
+        <div class="item" onclick="location.href='details.html';">
+            <div class="cau_left">
+                <img class="lazyOwl" data-src="images/c3.jpg" alt="Lazy Owl Image">
+            </div>
+            <div class="cau_left">
+                <h4><a href="details.html">branded jeens</a></h4>
+                <a href="details.html" class="btn">shop</a>
+            </div>
+        </div>
+    </div>
+    <!----//End-img-cursual---->
+</div>
+<!-- start main1 -->
+<div class="main_bg1">
+    <div class="wrap">
+        <div class="main1">
+            <h2>featured products</h2>
+        </div>
     </div>
 </div>
+<!-- start main -->
+<div class="main_bg">
+    <div class="wrap">
+        <div class="main">
+            <!-- start grids_of_3 -->
+            <div class="grids_of_3">
+                <div class="grid1_of_3">
+                    <a href="details.html">
+                        <img src="images/pic1.jpg" alt=""/>
+                        <h3>branded shoes</h3>
+                        <div class="price">
+                            <h4>$300<span>indulge</span></h4>
+                        </div>
+                        <span class="b_btm"></span>
+                    </a>
+                </div>
+                <div class="grid1_of_3">
+                    <a href="details.html">
+                        <img src="images/pic2.jpg" alt=""/>
+                        <h3>branded t-shirts</h3>
+                        <div class="price">
+                            <h4>$300<span>indulge</span></h4>
+                        </div>
+                        <span class="b_btm"></span>
+                    </a>
+                </div>
+                <div class="grid1_of_3">
+                    <a href="details.html">
+                        <img src="images/pic3.jpg" alt=""/>
+                        <h3>branded tees</h3>
+                        <div class="price">
+                            <h4>$300<span>indulge</span></h4>
+                        </div>
+                        <span class="b_btm"></span>
+                    </a>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="grids_of_3">
+                <div class="grid1_of_3">
+                    <a href="details.html">
+                        <img src="images/pic4.jpg" alt=""/>
+                        <h3>branded bags</h3>
+                        <div class="price">
+                            <h4>$300<span>indulge</span></h4>
+                        </div>
+                        <span class="b_btm"></span>
+                    </a>
+                </div>
+                <div class="grid1_of_3">
+                    <a href="details.html">
+                        <img src="images/pic5.jpg" alt=""/>
+                        <h3>ems women bag</h3>
+                        <div class="price">
+                            <h4>$300<span>indulge</span></h4>
+                        </div>
+                        <span class="b_btm"></span>
+                    </a>
+                </div>
+                <div class="grid1_of_3">
+                    <a href="details.html">
+                        <img src="images/pic6.jpg" alt=""/>
+                        <h3>branded cargos</h3>
+                        <div class="price">
+                            <h4>$300<span>indulge</span></h4>
+                        </div>
+                        <span class="b_btm"></span>
+                    </a>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!-- end grids_of_3 -->
+        </div>
+    </div>
+</div>
+<!-- start footer -->
