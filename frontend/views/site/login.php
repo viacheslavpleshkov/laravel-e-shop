28ab74a0f2d8dd45f1d36850caab387e9b568cfc
slava-pleshkov <slavapleshkovcom@gmail.com>
2017-12-31 19:05:33 +0200

    v-0-1-3

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -8,8 +8,39 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="content">
+    <div class="container">
+        <div class="login-page">
+            <div class="account_grid">
+                <div class="col-md-6 login-left wow fadeInLeft" data-wow-delay="0.4s">
+                    <h2>NEW CUSTOMERS</h2>
+                    <p>By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
+                    <a class="acount-btn" href="register.html">Create an Account</a>
+                </div>
+                <div class="col-md-6 login-right wow fadeInRight" data-wow-delay="0.4s">
+                    <h3>REGISTERED CUSTOMERS</h3>
+                    <p>If you have an account with us, please log in.</p>
+                    <form>
+                        <div>
+                            <span>Email Address<label>*</label></span>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <span>Password<label>*</label></span>
+                            <input type="password">
+                        </div>
+                        <a class="forgot" href="#">Forgot Your Password?</a>
+                        <input type="submit" value="Login">
+                    </form>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+    </div>
+
+
+
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
