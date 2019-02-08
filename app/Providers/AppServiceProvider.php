ca7fa2ffa051a1cc28f7ec3bcae0865ee2753bc2
Slava Pleshkov <mail@slavapleshkov.com>
2019-02-09 00:03:41 +0200

    Add heroku full

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+		if (config('app.env') === 'production') {
+			\URL::forceScheme('https');
+		}
     }
 
     /**
